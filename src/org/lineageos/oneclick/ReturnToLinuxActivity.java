0ab5618b42e048e9d7060a529de1eed209b389c7
Richard M <kronflux@gmail.com>
2017-01-23 01:37:30 -0700

    Create ReturnToLinuxActivity.java

---
+++ src/org/lineageos/oneclick/ReturnToLinuxActivity.java
@@ -0,0 +1,12 @@
+package org.lineageos.oneclick;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReturnToLinuxActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.returntolinux);
+    }
+}
