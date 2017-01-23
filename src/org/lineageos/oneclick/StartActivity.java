77a2efe1e24728d499df682f211ee3803de0e8ea
Richard M <kronflux@gmail.com>
2017-01-23 02:07:42 -0700

    Update StartActivity.java

--- src/org/lineageos/oneclick/StartActivity.java
+++ src/org/lineageos/oneclick/StartActivity.java
@@ -1,6 +1,12 @@
 package org.lineageos.oneclick;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,16 +15,31 @@ public class StartActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.welcome);
 
-        OneClickStats.sendEvent(this, OneClickStats.Categories.PAGE_SHOWN,
-            OneClickStats.Actions.PAGE_WELCOME);
+        // let's make sure play services is up to date, or analytics probably won't work
+        Integer resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0);
+            if (dialog != null) {
+                dialog.setOnDismissListener(new OnDismissListener() {
+                    @Override
+					public void onDismiss(DialogInterface dialog) {
+						// bail
+			            finish();
+					}
+				});
+
+                dialog.show();
+            }
+        }
+        
+        Analytics.sendEvent(getApplicationContext(), Analytics.STARTED);
 
         findViewById(R.id.begin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                OneClickStats.sendEvent(view.getContext(), OneClickStats.Categories.BUTTON_CLICK,
-                    OneClickStats.Actions.BTN_WELCOME);
                 startActivity(new Intent(StartActivity.this, DisclaimerActivity.class));
             }
         });
